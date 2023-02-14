71c0f1c3358a9a1deb3234506decbb535e6cefad
admin <anupandeyrp01@gmail.com>
2023-02-14 19:26:06 +0530

    Initial Commit

--- app/src/main/java/com/example/anu_exobenew/Splash_Screen.kt
+++ app/src/main/java/com/example/anu_exobenew/Splash_Screen.kt
@@ -1,7 +1,9 @@
 package com.example.anu_exobenew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,11 @@ class Splash_Screen : AppCompatActivity() {
         setContentView(R.layout.splash_screen)
 
 
+        Handler().postDelayed({
+            val intent= Intent(this@Splash_Screen,User_Login::class.java)
+            startActivity(intent)
+            finish()
+
+        },2000)
     }
 }
