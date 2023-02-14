f9fdcdae104c1772a4131ffa5a8b4b4a59844cfb
admin <anupandeyrp01@gmail.com>
2023-02-14 17:39:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/anu_exobenew/Splash_Screen.kt
@@ -0,0 +1,13 @@
+package com.example.anu_exobenew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/anu_exobenew/User_Login.kt
@@ -0,0 +1,11 @@
+package com.example.anu_exobenew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class User_Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_login)
+    }
+}
