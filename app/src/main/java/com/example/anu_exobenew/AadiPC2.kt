ff37ecb4f2a89d6a9c9b47bf8abcbfa385def7b2
Admin <anupandeyrp01@gmail.com>
2023-02-15 13:10:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/anu_exobenew/AadiPC2.kt
@@ -0,0 +1,11 @@
+package com.example.anu_exobenew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AadiPC2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aadi_pc2)
+    }
+}
